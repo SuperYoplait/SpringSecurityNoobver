3a3542255537a5af4b06ce560af389555ecfa03d
SuperYoplait <60890985+SuperYoplait@users.noreply.github.com>
2022-02-20 02:57:42 +0900

    초기세팅
    초기

---
+++ src/test/java/com/securiy/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securiy.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
